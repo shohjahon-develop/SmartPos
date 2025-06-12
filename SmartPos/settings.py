903df62658ce8a5dba5b1a1a67e8c3b8722c208f
shohjaxonmamajonov297@gmail.com <shohjaxonmamajonov297@gmail.com>
2025-06-12 07:23:25 -0700

    first commit

--- SmartPos/settings.py
+++ SmartPos/settings.py
@@ -64,7 +64,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 500
+    'PAGE_SIZE': 5000
 }
 
 
@@ -87,7 +87,7 @@ CORS_ALLOW_HEADERS = [
 CORS_ALLOWED_ORIGINS = [
     # Lokal test uchun
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
     "http://localhost:5173", # Vite uchun
 
     # Production uchun (eng muhimi shu qism)
